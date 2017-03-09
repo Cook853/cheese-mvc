865692b86fb683e53aebd79ad7dd6cc506729061
Andrew Cook <andrew.scott.cook.law@gmail.com>
2017-03-09 15:53:09 -0600

    Basic app, can add cheeses to list

---
+++ src/main/java/org/launchcode/Controllers/CheeseController.java
@@ -0,0 +1,43 @@
+package org.launchcode.Controllers;
+
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Lauren on 3/9/2017.
+ */
+@Controller
+@RequestMapping(value = "cheese")
+public class CheeseController {
+
+    static ArrayList<String> cheeses = new ArrayList<>();
+
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title", "My Cheeses");
+
+        return "cheese/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName) {
+        cheeses.add(cheeseName);
+
+//        redirect to /cheese, aka home, can specify other page after colon to redirect to
+        return "redirect:";
+    }
+}
